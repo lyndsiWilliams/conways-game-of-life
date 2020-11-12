e5b4e86a0638bf98918191e75dd8ece359eb1b6c
lyndsiWilliams <kcatgirl@gmail.com>
2020-11-12 17:31:38 -0600

    Removed console.logs and added media queries

--- game-of-life/src/components/Cell.js
+++ game-of-life/src/components/Cell.js
@@ -3,21 +3,21 @@ import React from 'react';
 
 
 const Cell = props => {
-    // Bring in x and y position and cellSize as props
-    const { x, y, cellSize } = props;
+  // Bring in x and y position and cellSize as props
+  const { x, y, cellSize } = props;
 
-    return (
-        <div
-            className="cell"
-            // This sets the dimensions of the cell
-            style={{
-                left: `${cellSize * x + 1}px`,
-                top: `${cellSize * y + 1}px`,
-                width: `${cellSize - 1}px`,
-                height: `${cellSize - 1}px`,
-            }}
-        />
-    );
+  return (
+    <div
+      className="cell"
+      // This sets the dimensions of the cell
+      style={{
+        left: `${cellSize * x + 1}px`,
+        top: `${cellSize * y + 1}px`,
+        width: `${cellSize - 1}px`,
+        height: `${cellSize - 1}px`,
+      }}
+    />
+  );
 };
 
 export default Cell;
--- game-of-life/src/components/Rules.js
+++ game-of-life/src/components/Rules.js
@@ -3,22 +3,29 @@ import React from 'react';
 
 
 const Rules = () => {
-    return (
-        <div>
-            <h2>How to play:</h2>
-                <li>Click some squares (or the <span>Random button</span>) to populate the screen.</li>
-                <li>Set the <span>speed</span> of the game (in milliseconds)</li>
-                <li>Click the <span>Smaller/Larger buttons</span> to set the cell size.</li>
-                <li>Click the <span>Run button</span> to start the game!</li>
-                <li>Click the <span>Stop button</span> to stop the simulation.</li>
-                <li>Click the <span>Clear button</span> to reset the board.</li>
+  return (
+    <div className="rules-wrapper">
+      <div style={{ marginRight: "1%" }}>
+        <h2>How to play:</h2>
+        <section style={{ textAlign: "left" }}>
+          • Click some squares (or the <span>Random button</span>) to populate the screen.<br/>
+          • Set the <span>speed</span> of the game (in milliseconds)<br/>
+          • Click the <span>Smaller/Larger buttons</span> to set the cell size.<br/>
+          • Click the <span>Run button</span> to start the game!<br/>
+          • Click the <span>Stop button</span> to stop the simulation.<br/>
+          • Click the <span>Clear button</span> to reset the board.<br/>
+        </section>
+      </div>
 
-            <h2>Rules:</h2>
-                <li>If the cell is <span>alive</span> and has 2 or 3 neighbors, it remains <span>alive</span>. Otherwise it <span>dies</span>.</li>
-                <li>If the cell is <span>dead</span> and has exactly 3 neighbors, then it comes to <span>life</span>.</li>
-
-        </div>
-    )
-}
+      <div className="rules-section">
+        <h2>Rules:</h2>
+        <section style={{ textAlign: "left" }}>
+          • If the cell is <span>alive</span> and has 2 or 3 neighbors, it remains <span>alive</span>. Otherwise it <span>dies</span>.<br/><br/>
+          • If the cell is <span>dead</span> and has exactly 3 neighbors, then it comes to <span>life</span>.<br/>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Rules;
